00dd88f7fb49419f1dc9e3a04586a996d5537ef4
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2017-12-30 14:26:40 +0100

    Quizz view added some style

--- components/QuizzView.js
+++ components/QuizzView.js
@@ -1,89 +1,100 @@
 import React from 'react'
 import {
-	StyleSheet,
-	View,
-	Text,
+    StyleSheet,
+    View,
+    Text,
     TouchableOpacity,
-	FlatList
+    FlatList
 } from 'react-native'
 
 class QuizzView extends React.Component {
 
-	constructor() {
-		super();
-		this.state = {
-			questions: [],
-			currentQuestion: 0
-		}
-	}
+    constructor() {
+        super();
+        this.state = {
+            questions: [],
+            currentQuestion: 0
+        }
+    }
 
-	componentDidMount() {
-		console.log('quizzView', this.props.navigation.state.params)
-		this.setState({
-		    count: `${this.state.currentQuestion + 1} / ${this.props.navigation.state.params.length}`,
-		    questions: this.props.navigation.state.params
-		})
-	}
+    componentDidMount() {
+        console.log('quizzView', this.props.navigation.state.params)
+        this.setState({
+            count: `${this.state.currentQuestion + 1} / ${this.props.navigation.state.params.length}`,
+            questions: this.props.navigation.state.params
+        })
+    }
 
-	/*renderItem = ({ item }) => (
-		<View style={styles.item}>
-			<TouchableOpacity
-				onPress={() => {
-					this.props.navigation.navigate('QuizzView', item)
-				}}>
-				<Text style={{textAlign: 'center'}}>Question: {item.question}</Text>
-			</TouchableOpacity>
-		</View>
-	)*/
-
-
-	// _keyExtractor = (item, i) => i;
-
-	render() {	
+    render() {
         console.log('this.state: ', this.state)
-		return (
-			<View>
-				<View style={styles.container}>
-					<Text>QUIZZ</Text>
-				</View>
+        return (
+            <View>
+                <View style={styles.container}>
+                    <Text>QUIZZ</Text>
+                </View>
                 <View>
-					<Text>{this.state.count}</Text>
-				</View>
-                <View>
+                    <Text >{this.state.count}</Text>
+                </View>
+                <View >
                     {this.state.questions.length > 0 &&
-                        <Text>{this.state.questions[this.state.currentQuestion].question}</Text>
+                        <Text style={{textAlign: 'center'}}>{this.state.questions[this.state.currentQuestion].question}</Text>
                     }
                 </View>
-                <View>
-                    <Text>Answer</Text>
+                <View style={styles.viewAnswer}>
+                    <Text style={{textAlign: 'center', color: 'red'}}>View Answer</Text>
                 </View>
                 <View>
-                    <TouchableOpacity>
-                        <Text>Correct</Text>
+                    <TouchableOpacity style={[styles.button, styles.buttonCorrect]}>
+                        <Text style={{textAlign: 'center', color: 'white'}}>Correct</Text>
                     </TouchableOpacity>
                 </View>
                 <View>
-                    <TouchableOpacity>
-                        <Text>Incorrect</Text>
+                    <TouchableOpacity style={[styles.button, styles.buttonIncorrect]}>
+                        <Text style={{textAlign: 'center', color: 'white'}}>Incorrect</Text>
                     </TouchableOpacity>
                 </View>
-			</View>
-		)
-	}
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-	container: {
+    container: {
         backgroundColor: 'white',
-		padding: 10,
-		flexDirection: 'row',
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    deck: {
+        width: 400,
+        margin: 0,
+        padding: 0,
+    },
+    viewAnswer: {
+        justifyContent: 'center',
+        margin: 20
+    },  
+    button: {
+		borderRadius: 16,
+		padding: 20,
+		marginLeft: 10,
+		marginRight: 10,
+		marginTop: 25,
 		justifyContent: 'center',
+		shadowRadius: 3,
+		shadowOpacity: 0.8,
+		shadowColor: 'rgba(0, 0, 0, 0.24)',
+		shadowOffset: {
+			width: 0,
+			height: 3
+		}
 	},
-	deck: {
-		width: 400,
-		margin: 0,
-		padding: 0,
-	},
+    buttonCorrect: {
+        backgroundColor: 'green'
+    },
+    buttonIncorrect: {
+        backgroundColor: 'red'
+    },
 })
 
 export default QuizzView;
